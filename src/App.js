ffdf79c1f7d1dda1546122d6facb3ca4ab011d7d
Giraffe_sungjun <kis07150@naver.com>
2023-10-14 13:38:20 +0900

    ABF-21 fix : 상단바 UI 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-// or less ideally
-import { Button } from 'react-bootstrap';
-import Topbar from "./components/Topbar";
+import './App.css';
+import MainPage from "./pages/MainPage";
+import BlogPage from "./pages/BlogPage";
+import ManagePage from "./pages/ManagePage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
+
+  const marginStyle = {
+    marginLeft: '3rem',
+    marginRight: '3rem',
+  }
+    
   return (
-    <div>
-      <Topbar />
+    
+    <div style={marginStyle}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/blogs" element={<BlogPage />} />
+          <Route path="/mngt" element={<ManagePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
