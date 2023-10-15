607533ff0ef4e0f7d75466732df152c84997db40
minjun0707 <75360915+minjun0707@users.noreply.github.com>
2023-10-15 21:20:52 +0900

    ABF-9 feat : Route 요소 변경

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MainPage from "./pages/MainPage";
 import BlogPage from "./pages/BlogPage";
 import ManagePage from "./pages/ManagePage";
 import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/sign-up" element={<ignUpPage />} />
+          <Route path="/sign-up" element={<SignUpPage />} />
           <Route path="/blogs" element={<BlogPage />} />
           <Route path="/mngt" element={<ManagePage />} />
         </Routes>
