f773151ba5b4b1f66f4308764985dd8324c1a88a
changmeen <fbckdals99@gmail.com>
2023-10-16 18:50:03 +0900

    ABF-20/feat/blog-post-view BlogTopbar, BlogSidebar, Footer, 글 작성하기 버튼 추가

--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,11 +1,26 @@
-import BlogTopSideBar from "../components/BlogTopSideBar";
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import BlogTopbar from "../components/BlogTopbar";
+import BlogBody from "../components/BlogBody";
+import BlogSidebar from "../components/BlogSidebar";
+import Footer from "../components/Footer";
 
 const BlogPage = () => {
     return (
-        <div>
-            <BlogTopSideBar />
-        </div>
-    )
-}
+      <div>
+        <BlogTopbar />
+        <hr />
+        <Row>
+          <Col md={2}>
+            <BlogSidebar />
+          </Col>
+          <Col md={10}>
+            <BlogBody />
+          </Col>
+        </Row>
+         <Footer />
+      </div>
+    );
+  };
 
 export default BlogPage;
