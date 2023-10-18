21813cf1c88bd9cb488c103b256c8474e15b2fd5
Rhew Changmeen <fbckdals99@gmail.com>
2023-10-18 15:05:31 +0900

    ABF-27 design PostPage 디자인 변경

--- src/components/PostEditor/PostEditor.js
+++ src/components/PostEditor/PostEditor.js
@@ -5,6 +5,8 @@ import { EditorState, convertToRaw } from "draft-js";
 import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
 import draftjsToHtml from "draftjs-to-html";
 import ThumbnailModal from "../ThumbnailModal/ThumbnailModal.js"
+import {TextField} from '@mui/material/';
+import {MenuItem} from '@mui/material/';
 
 const Container = styled.div`
   width: 100%;
@@ -35,30 +37,71 @@ const PostEditor = () => {
     console.log("이미지 업로드");
   };
 
+  const currencies = [
+    {
+      value: '카테고리1'
+    },
+    {
+      value: '카테고리2'
+    },
+    {
+      value: '카테고리3'
+    },
+    {
+      value: '카테고리4'
+    },
+  ];
+
   return (
     <>
-      <Container>
-        <Editor
-          placeholder="게시글을 작성해주세요"
-          editorState={editorState}
-          onEditorStateChange={updateTextDescription}
-          toolbar={{
-            image: { uploadCallback: uploadCallback },
-          }}
-          localization={{ locale: "ko" }}
-          editorStyle={{
-            height: "400px",
-            width: "100%",
-            border: "3px solid lightgray",
-            padding: "20px",
-          }}
-        />
-      </Container>
-      <button onClick={()=>setShowModal(true)}>작성 완료</button>
-      <ThumbnailModal
-        showModal={showModal}
-        onClose={() => setShowModal(false)}
+    <div style={{marginTop: '30px', marginBottom: '15px'}}>
+    <TextField
+      id="post-category"
+      select
+      label="게시글 카테고리"
+      defaultValue='카테고리1'
+      style={{width: 'auto'}}>
+      {currencies.map((option) => (
+        <MenuItem key={option.value} value={option.value}>
+          {option.value}
+        </MenuItem>
+      ))}
+    </TextField>
+    </div>
+
+    <div style={{marginTop: '15px', marginBottom: '30px'}}>
+    <TextField
+      id="post-title"
+      label="게시글 제목"
+      variant="outlined"
+      style={{width: '100%'}}>
+    </TextField>
+    </div>
+    
+    <Container>
+      <Editor
+        placeholder="게시글을 작성해주세요"
+        editorState={editorState}
+        onEditorStateChange={updateTextDescription}
+        toolbar={{
+          image: { uploadCallback: uploadCallback },
+        }}
+        localization={{ locale: "ko" }}
+        editorStyle={{
+          height: "400px",
+          width: "100%",
+          border: "3px solid lightgray"
+        }}
       />
+    </Container>
+
+    <div style={{ display: "flex", justifyContent: "space-between" }}>
+      <div></div>
+      <button style={{marginTop: '30px'}} onClick={()=>setShowModal(true)}>작성 완료</button>
+    </div>
+    <ThumbnailModal
+      showModal={showModal}
+      onClose={() => setShowModal(false)}/>
     </>
   );
 };
