de964c7f3ea2e2e242d10f0c48a5b32c137c300a
suha <hp980724@gmail.com>
2023-10-18 15:24:11 +0900

    ABF-25 design : 공통 css 구축

---
+++ src/common.js
@@ -0,0 +1,26 @@
+import styled from "@emotion/styled";
+import { Button } from "@mui/material";
+
+export const CommonButton = styled(Button)`
+    text-align: center;
+    width: 156px;
+    height: 48px;
+    line-height: 48px;
+    background: #00b4ef;
+    border: 1px solid #dadada;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 15px;
+    font-style: normal;
+    font-weight: 400;
+    font-size: 18px;
+    color: white;
+    cursor: pointer;
+    transition: all 0.5s ease;
+
+    &:hover {
+    transform: scale(1.05);
+    background: cornflowerblue;
+    transition: 0.5s;
+    }
+`; 
+
