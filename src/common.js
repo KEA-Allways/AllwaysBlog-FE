72faf32d84e85f011d9d3f711393a9b0b0290c94
Dajeong Choe <hers010801@gmail.com>
2023-10-28 23:42:12 +0900

    ABF-70 design : 블로그 관리 페이지 버튼 수정

--- src/common.js
+++ src/common.js
@@ -27,6 +27,7 @@ export const CommonColorButton = styled(Button)`
     width: 156px;
     height: 48px;
     border: 1px solid #dadada;
+    text-align: center;
     font-style: normal;
     font-weight: 400;
     font-size: 18px;
@@ -35,7 +36,7 @@ export const CommonColorButton = styled(Button)`
     display: flex;
     justify-content: center;
     align-items: center;
-    margin-right: 20px;
+    //margin-right: 20px;
     border-radius: 10px;
     box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.1);
     transition: all 0.5s ease;
