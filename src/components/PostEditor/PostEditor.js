5feadb4dd64d35c930e65d67bf9fdcc77ad6f4a5
allrightDJ0108 <hers010801@gmail.com>
2023-11-21 14:59:34 +0900

    ABF-108 fix : 에디터 작성 중 이미지 첨부 시 S3 자동 저장

--- src/components/PostEditor/PostEditor.js
+++ src/components/PostEditor/PostEditor.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef, useMemo } from "react";
 import styled from "styled-components";
 import { Editor } from "react-draft-wysiwyg";
 import axios from "axios";
@@ -16,6 +16,12 @@ import TextStyles from "../../components/Text.module.css";
 import ReactQuill from "react-quill";
 //import QuillEditor from "../editor.js";
 import "react-quill/dist/quill.snow.css";
+import AWS from "aws-sdk";
+
+const REACT_APP_AWS_S3_BUCKET_REGION = 'ap-northeast-2';
+const REACT_APP_AWS_S3_BUCKET_ACCESS_KEY_ID = 'AKIA5DFK2SNGSWA4F2CW';
+const REACT_APP_AWS_S3_BUCKET_SECRET_ACCESS_KEY = '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';
+
 
 const Container = styled.div`
   width: 100%;
@@ -30,16 +36,6 @@ const PostEditor = () => {
   const templateSeq = location.state.templateSeq;
   console.log("postSeq: " + postSeq);
   console.log("TemplateSeq: " + templateSeq);
-
-  const modules = {
-    toolbar: {
-      container: [
-        ["image"],
-        [{ header: [1, 2, 3, 4, 5, false] }],
-        ["bold", "underline"],
-      ],
-    },
-   };
 
    const [content, setContent] = useState("");
 console.log(content);
@@ -73,8 +69,114 @@ console.log(content);
   };
   const handlePostComplete = () => {
     setShowModal(true); // 작성 완료 버튼 클릭 시 모달 표시
+
+    fetch('http://localhost:8085/api/posts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'userSeq' : 1,
+        },
+        body: JSON.stringify({
+          postTitle: titleState,
+          postContent: content,
+          categorySeq : 1
+        }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+
     console.log('Post complete button clicked'); // 확인용 로그
   };
+
+  const handlePostComplete2 = () => {
+    setShowModal(true); // 작성 완료 버튼 클릭 시 모달 표시
+
+    fetch('http://localhost:8085/api/posts/23', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'userSeq' : 1,
+        },
+        body: JSON.stringify({
+          postTitle: titleState,
+          postContent: content,
+          categorySeq : 1
+        }),
+      })
+        .then(response => response.json())
+        .then(data => {
+          console.log(data);
+        })
+        .catch(error => {
+          console.error('Error:', error);
+        });
+
+    console.log('Post complete button clicked'); // 확인용 로그
+  };
+
+  const quillRef = useRef(null);
+  const imageHandler = async () => {
+    const input = document.createElement("input");
+    input.setAttribute("type", "file");
+    input.setAttribute("accept", "image/*");
+    input.click();
+    input.addEventListener("change", async () => {
+      //이미지를 담아 전송할 file을 만든다
+      const file = input.files?.[0];
+      try {
+        //업로드할 파일의 이름으로 Date 사용
+        const name = Date.now();
+        //생성한 s3 관련 설정들
+        AWS.config.update({
+          region: REACT_APP_AWS_S3_BUCKET_REGION,
+          accessKeyId: REACT_APP_AWS_S3_BUCKET_ACCESS_KEY_ID,
+          secretAccessKey: REACT_APP_AWS_S3_BUCKET_SECRET_ACCESS_KEY,
+          
+        });
+        //앞서 생성한 file을 담아 s3에 업로드하는 객체를 만든다
+        const upload = new AWS.S3.ManagedUpload({
+          params: {
+            ACL: "public-read",
+            Bucket: "allways-test-bucket", //버킷 이름
+            Key: `upload/${name}.${file.type.split("/")[1]}`,
+            Body: file,
+            ContentType: file.type,
+          },
+        });
+        //이미지 업로드 후
+        //곧바로 업로드 된 이미지 url을 가져오기
+        const IMG_URL = await upload.promise().then((res) => res.Location);
+        //useRef를 사용해 에디터에 접근한 후
+        //에디터의 현재 커서 위치에 이미지 삽입
+        const editor = quillRef.current.getEditor();
+        const range = editor.getSelection();
+        // 가져온 위치에 이미지를 삽입한다
+        editor.insertEmbed(range.index, "image", IMG_URL);
+      } catch (error) {
+        console.log(error);
+      }
+    });
+  };
+
+  const modules = useMemo(() => {
+    return {
+      toolbar: {
+        container: [
+          ["image"],
+          [{ header: [1, 2, 3, 4, 5, false] }],
+          ["bold", "underline"],
+        ],
+        handlers: {
+          image: imageHandler,
+        },
+      },
+    };
+  }, []);
 
   const uploadCallback = () => {
     console.log("이미지 업로드");
@@ -225,7 +327,8 @@ console.log(content);
       </div>
 
         <ReactQuill
-          style={{ width: "100%", height: "300px", border: "" }}
+          ref={quillRef}
+          style={{ width: "100%", height: "600px" }}
           modules={modules}
           onChange={setContent}
           placeholder="게시글을 작성해주세요"
@@ -252,6 +355,8 @@ console.log(content);
       <ThumbnailModal
         showModal={showModal}
         onClose={() => setShowModal(false)} />
+      <CommonButton onClick={() => handlePostComplete()}> INSERT TEST</CommonButton>
+      <CommonButton onClick={() => handlePostComplete2()}> UPDATE TEST</CommonButton>
     </>
   );
 };
